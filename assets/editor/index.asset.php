009ef4840208c0926ab6e20798c6a67f4b805654
Marko Krstic <mailme@markokrstic.com>
2026-04-03 10:53:05 +0200

    ..

--- assets/editor/index.asset.php
+++ assets/editor/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'fc6093ce7be28d648859');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '71dab580fb576db66c4e');
